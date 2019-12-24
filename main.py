4bc0603e6dca95f27413977400c05ac9a3d57555
narhz <narhz@pm.me>
2019-12-24 12:54:38 -0600

    add selection loop

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import click
 from pprint import pprint
 import funcs
 
+import os
+
+
+
 @click.command()
 @click.option('--search')
 @click.option('--count', default=5)
@@ -11,8 +15,19 @@ import funcs
 def temp(search, count):
     keywords = funcs.search(search, count)
 
+    urls = []
     for item in keywords:
         print(str(item.get('index')) + ' - ' + item.get('title'))
+        urls.append(item.get('video-url'))
+        
+    while True:
+        sel = click.prompt('> ')
 
-
+        if sel == 'exit':
+            break
+        elif not sel:
+            print('!')
+        else:
+            os.system('mpv ' + urls[int(sel)])
+    
 temp()
