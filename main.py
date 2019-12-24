7bc509f1e328a012e695b96289aeafe25a431b29
narhz <narhz@pm.me>
2019-12-24 11:14:27 -0600

    define temp function

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 import click
 from pprint import pprint
-
+import funcs
 
 @click.command()
 @click.option('--search')
 @click.option('--count', default=5)
 
-def temp():
-    pass
+def temp(search, count):
+    keywords = funcs.search(search, count)
+
+    for item in keywords:
+        print(str(item.get('index')) + ' - ' + item.get('title'))
+
+
+temp()
